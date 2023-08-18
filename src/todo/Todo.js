d17a8812b97de9d7c2b8828dbdbec8a655a9191b
bhanu <bhanupsk2000@gmail.com>
2023-08-18 15:36:05 +0530

    function Added

--- src/todo/Todo.js
+++ src/todo/Todo.js
@@ -2,22 +2,75 @@ import React, { useState } from 'react'
 import '../App.css';
 import data from './todoData.json'
 const Todo = () => {
-  const [list ,setList]=useState(data)
-  // const [const ]
-  console.log(list)
+  const [list, setList] = useState(data)
+  const [msg, setmsg] = useState('');
+  console.log(msg)
+
+  
+  // adding task
+  const addTask =()=>{
+    let tempList=JSON.parse(JSON.stringify(list));
+    let singleData={
+      id:list.length+1,
+      msg:msg,
+      status:"doing"
+    }
+    tempList.push(singleData);
+    setmsg('')
+    setList(tempList)
+  }
+
+  const markDoing=(id)=>{
+    let tempList=JSON.parse(JSON.stringify(list));
+    tempList.forEach((val) => {
+      if(val.id===id)
+      val.status="doing"
+    });
+    setList(tempList)
+  }
+
+  const markDone=(id)=>{
+    let tempList=JSON.parse(JSON.stringify(list));
+    tempList.forEach((val) => {
+      if(val.id===id)
+      val.status="done"
+    });
+    setList(tempList)
+  }
+
+  
   return (
     <div className='Contener'>
       <div className='New'>
         <h3>Creat New Task</h3>
-        <input></input>
-        <button>Button</button>
+        <input value={msg} onChange={(e) => { setmsg(e.target.value) }}></input>
+        <button onClick={()=>{addTask()}}>Button</button>
       </div>
       <div className='Do'>
         <h1>To Do</h1>
-        <input type="checkbox"></input>
+        {
+          list.filter((item)=>item.status==='doing').map((val) => {
+            return (
+              <div>
+                <input type="checkbox" onClick={()=>{markDone(val.id)}}></input>
+                <span>{val.msg}</span>
+              </div>
+            )
+          })
+        }
       </div>
       <div className='Com'>
         <h3>Complited Task</h3>
+        {
+          list.filter((item)=>item.status==='done').map((val) => {
+            return (
+              <div>
+                <input type="checkbox" checked onClick={()=>{markDoing(val.id)}}></input>
+                <span>{val.msg}</span>
+              </div>
+            )
+          })
+        }
       </div>
     </div>
   )
